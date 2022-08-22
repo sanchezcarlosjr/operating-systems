7764983c5037014cfe6df9aea75b3406e7663ae1
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-08-22 13:06:03 -0700

    feat: test fork

--- fork.c
+++ fork.c
@@ -3,25 +3,34 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-	int i, j, pid, salida;
+void executeChildProcess(const char* pathname, int i) {
+	if (fork() != 0) {
+		printf("Parent again! (pid:%d)\n", (int) getpid());	
+		return;
+	}
+	unsigned int fileDescriptor;
 	char c;
-	for (i=1; i<argc; i++) {
+	int j;
+	bool isError = (fileDescriptor = open(pathname, O_CREAT | O_WRONLY | O_TRUNC, 0644)) < 0;
+	if(isError) {
+		write(2, "Error\n", 6);
+		exit(1);
+	}
+	printf("\t Child (pid:%d)\n", (int) getpid());	
+	for (j=0; j < 100000; j++) {
+		c='A' + i - 1;
+		write(fileDescriptor, &c, 1);
+	}
+	printf("\t I've finish. Child (pid:%d)\n", (int) getpid());	
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+	sleep(5);
+	for (int i=1; i<argc; i++) {
 		printf("%d,", i);
-		printf("hello, I am parent (pid:%d)?\n", (int) getpid());	
-		if ((pid=fork())==0) {
-			if ((salida = open(argv[i], O_CREAT | O_WRONLY | O_TRUNC, 0644)) < 0) {
-				write(2, "Error\n", 6);
-				exit(1);
-			}
-			printf("hello, I am child (pid:%d)\n", (int) getpid());	
-			for (j=0; j < 99; j++) {
-				c= 'A' + i - 1;
-				write(salida, &c, 1);
-			}
-			sleep(10);
-			exit(0);
-		}
+		printf("Parent (pid:%d)\n", (int) getpid());	
+		executeChildProcess(argv[i], i);
 	}
 	exit(0);
 }
