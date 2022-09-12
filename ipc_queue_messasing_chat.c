ba752bd8a75b9628d28eac090eaa758dc9ae4e80
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-09-12 14:44:58 -0700

    feat: operating system

--- ipc_queue_messasing_chat.c
+++ ipc_queue_messasing_chat.c
@@ -2,25 +2,40 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <string.h>
-// structure for message queue
+#include <stdlib.h> 
+#include <unistd.h>
+#include <signal.h>
+
 struct Message {
-    long mesg_type;
-    char mesg_text[100];
+	long mesg_type;
+	char mesg_text[100];
 } message;
- 
-int main()
-{
-    key_t key = ftok("progfile", 65);
-    int msgid = msgget(key, 0666 | IPC_CREAT);
-    
-    do {
-	    message.mesg_type = 1;
-	    printf(">");
-	    fgets(message.mesg_text, sizeof(message.mesg_text)/sizeof(char), stdin);
-	    msgsnd(msgid, &message, sizeof(message), 0);
-	    printf("You: %s", message.mesg_text);
-    } while(strcmp(message.mesg_text, "fin de la transmision") != 0);
-    msgctl(msgid, IPC_RMID, NULL);
-    return 0;
+
+#define MESSAGE_EOF "fin de la transmision\n"
+
+int main(int argc, char* argv[]) {
+	key_t key = ftok("progfile", 65);
+	int msgid = msgget(key, 0666 | IPC_CREAT);
+	pid_t c_pid = -1;
+	if ((c_pid = fork()) == 0) {
+		msgrcv(msgid, &message, sizeof(message), atoi(argv[2]), IPC_NOWAIT);
+		do {
+			msgrcv(msgid, &message, sizeof(message), atoi(argv[2]), 0);
+			printf("\n%s:%s>\n",  argv[2], message.mesg_text);
+			fflush(stdin);
+		} while(strcmp(message.mesg_text, MESSAGE_EOF));
+		kill(getppid(), SIGKILL);
+		exit(0);
+	} else {
+		message.mesg_type = atoi(argv[1]);
+		do {
+			printf(">");
+			fgets(message.mesg_text, 100, stdin);
+			msgsnd(msgid, &message, sizeof(message), 0);
+		} while(strcmp(message.mesg_text,  MESSAGE_EOF));
+		kill(c_pid, SIGKILL);
+		msgctl(msgid, IPC_RMID, NULL);
+	}
+	return 0;
 }
 
