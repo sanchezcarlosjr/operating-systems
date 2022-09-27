aad9c34117f9117ab8df3f33c7ed1bf7bf9ac41a
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-09-27 11:37:03 -0700

    fix: socket to peer

--- posix_chat_tcp_ipv4_sockets/chat.cpp
+++ posix_chat_tcp_ipv4_sockets/chat.cpp
@@ -64,16 +64,12 @@ class Done: public State {
 class ActiveSocket: public State {
 public:
 	State* transite(Socket* socket) {
-		std::thread thread(&ActiveSocket::receive, this, socket);
-		while(true) {
-			char buffer[BUFFER_SIZE];
-			if (recv(socket->peerDescriptor, buffer, BUFFER_SIZE, 0) < 0) {
-				printf("\r[ERROR] Reception failed.\n");
-				exit(EXIT_FAILURE);
-			}
-			printf("\rPeer: %s\n", buffer);
-			sleep(2);
+		char buffer[BUFFER_SIZE];
+		if (recv(socket->peerDescriptor, buffer, BUFFER_SIZE, 0) < 0) {
+			printf("\r[ERROR] Reception failed.\n");
+			exit(EXIT_FAILURE);
 		}
+		printf("\rPeer: %s\n", buffer);
 		return new Done();
 	}
 
@@ -87,19 +83,17 @@ public:
 	State* transite(Socket* socket) {
 		std::thread thread(&PassiveSocket::receive, this, socket);
 		char message[BUFFER_SIZE];
-		while(true) {
-			printf("\n>", message);
-			scanf("% [^\n]s", message);
-			size_t messageLength = strlen(message);
-			ssize_t numBytes = send(socket->socketDescriptor, message, messageLength, 0);
-			if (numBytes < 0) {
-				fputs("\r[ERROR] send failed\n", stderr);
-				exit(EXIT_FAILURE);
-			}
-			if (numBytes != messageLength) {
-				fputs("\r[ERROR] send un expected number of bytes\n", stderr);
-				exit(EXIT_FAILURE);
-			}
+		printf("\n>", message);
+		scanf("% [^\n]s", message);
+		size_t messageLength = strlen(message);
+		ssize_t numBytes = send(socket->socketDescriptor, message, messageLength, 0);
+		if (numBytes < 0) {
+			fputs("\r[ERROR] send failed\n", stderr);
+			exit(EXIT_FAILURE);
+		}
+		if (numBytes != messageLength) {
+			fputs("\r[ERROR] send un expected number of bytes\n", stderr);
+			exit(EXIT_FAILURE);
 		}
 		return new Done();
 	}
