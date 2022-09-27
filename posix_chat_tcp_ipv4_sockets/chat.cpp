751fa1c2d05af1989292e48cd410da4696a6d0ff
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-09-27 12:58:02 -0700

    fix: socket to peer

--- posix_chat_tcp_ipv4_sockets/chat.cpp
+++ posix_chat_tcp_ipv4_sockets/chat.cpp
@@ -62,7 +62,7 @@ class Done: public State {
 class KindSocket: public State {
 public:
 	virtual State* transite(Socket* socket) {
-		printf("\rYou have connected succesful to %s:%d\n>", socket->peerName, ntohs(socket->peerAddress.sin_port));
+		printf("\rYou have connected succesful to %s:%d\n", socket->peerName, ntohs(socket->peerAddress.sin_port));
 		std::thread t(&KindSocket::receiveMessages, this, socket);
 		sendMessages(socket);
 		return new Done();
@@ -71,6 +71,7 @@ public:
 	virtual int from(Socket* socket) = 0;
 	void sendMessages(Socket* socket) {
 		char* message;
+		printf("\r>");
 		while(true) {
 			printf("\r");
 			scanf(" %m[^\n]s", &message);
