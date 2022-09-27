747b765d0c72e64d3c8376637d980935a49e1a3c
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-09-27 13:41:35 -0700

    feat: list ip address

--- posix_chat_tcp_ipv4_sockets/chat.cpp
+++ posix_chat_tcp_ipv4_sockets/chat.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ifaddrs.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -123,6 +124,35 @@ public:
 	}
 };
 
+// https://stackoverflow.com/questions/212528/how-can-i-get-the-ip-address-of-a-linux-machine
+void showIpAddress() {
+	struct ifaddrs * ifAddrStruct=NULL;
+	struct ifaddrs * ifa=NULL;
+	void * tmpAddrPtr=NULL;
+
+	getifaddrs(&ifAddrStruct);
+
+	for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+		if (!ifa->ifa_addr) {
+			continue;
+		}
+		if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
+							   // is a valid IP4 Address
+			tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+			char addressBuffer[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+			printf("\r%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
+		} else if (ifa->ifa_addr->sa_family == AF_INET6) { // check it is IP6
+								   // is a valid IP6 Address
+			tmpAddrPtr=&((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+			char addressBuffer[INET6_ADDRSTRLEN];
+			inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
+			//printf("\r%s IP Address %s\n", ifa->ifa_name, addressBuffer); 
+		} 
+	}
+	if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+}
+
 class BindingSocket: public State {
 	State* transite(Socket* s) {
 		s->socketAddress = {
@@ -148,6 +178,7 @@ class BindingSocket: public State {
 			exit(EXIT_FAILURE);
 		}
 		s->socketPort = ntohs(sin.sin_port);
+		showIpAddress();
 		printf("\rListening on port %d\n", s->socketPort);
 
 		s->peerAddress = *(new sockaddr_in);
