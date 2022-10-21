aff2a987e3d81ded0fa17a60eb3e6eddf5e1b52a
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-10-21 08:53:49 -0700

    refactor: new comments

--- memory_prog1.c
+++ memory_prog1.c
@@ -1,13 +1,10 @@
-// $ gcc prog01.c -o prog01
-// $ size prog01
+// $ gcc memory_prog01.c -o prog01.out
+// $ size prog01.out
 
 #include <stdio.h>
 
 
 int main(void)
-
 {
-
-return 0;
-
+	return 0;
 }
